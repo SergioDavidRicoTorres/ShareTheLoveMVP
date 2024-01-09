37a7ae6157fe57a705306b51ba07541a435faa03
Sergio David Rico Torres <sergiodavidricotorres@eduroam-077-080-121-129.wlan.univie.ac.at>
2024-01-09 14:22:46 +0100

    add secrets.tsx

---
+++ secrets.tsx
@@ -0,0 +1,11 @@
+export const spotifyCredentials = {
+  clientId: "7e18ac3973ba4d51a66bfe7265a75fb1",
+  clientSecret: "dd73ec8705e94cf98551ff5ebe48d639",
+  redirectUri: "com.domain.ShareTheLove1234://redirect",
+};
+
+export const TMDBCredentials = {
+  api_key: "45dc205ebbe32bd29ac3ccc8e36cae2d",
+  api_read_access_token:
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NWRjMjA1ZWJiZTMyYmQyOWFjM2NjYzhlMzZjYWUyZCIsInN1YiI6IjY0NzhiZjM4OTM4MjhlMDBmOWQ2MTNjYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ugiFgE_NeI3N31PSX2_fk1SDugml3BWg4Q7KMCt_Bm0",
+};
